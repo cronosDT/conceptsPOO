206649c0cabd7d0e05354b0fc71869bb39cd6ae6
= <=>
2022-02-12 19:56:53 -0500

    First commit about days and their methods

---
+++ conceptsPOO/concepstPOO/concepstPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using concepstPOO;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("------------------------------------");
+
+try
+{
+    Console.WriteLine(new Date(1998, 1, 3));
+    Console.WriteLine(new Date(1554, 12, 30));
+    Console.WriteLine(new Date(1887, 2, 30));
+    Console.WriteLine(new Date(1999, 11, 10));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
